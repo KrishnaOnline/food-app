a89d6efe9eb77b73965043a25a63f0530a8dc72c
KrishnaOnline <krishnavamshikusuma@gmail.com>
2023-08-28 08:10:44 +0530

    routes attatched

--- src/Components/RestaurantMenu.js
+++ src/Components/RestaurantMenu.js
@@ -58,7 +58,7 @@ const RestaurantMenu = () => {
     }
 
     if(!menu) {
-        return (<h1>Sorry! Menu of this Restaurant is NOT AVAILABLE at the Moment</h1>)
+        return (<h1>Sorry! Menu of this Restaurant is NOT AVAILABLE at the Moment, Check Other Restaurants</h1>)
     }
 
     return (
